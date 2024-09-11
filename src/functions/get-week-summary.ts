327de11f7d79cbd3af43cd2c8942b2630db52393
Vitor Andes <vitor.andes.santos04@gmail.com>
2024-09-11 16:46:08 -0300

    last commit backend, routes and functions

---
+++ src/functions/get-week-summary.ts
@@ -0,0 +1,82 @@
+import dayjs from "dayjs";
+import { and, eq, gte, lte, sql } from "drizzle-orm";
+import { db } from "../db";
+import { goalCompletions, goals } from "../db/schema";
+
+export async function getWeekSummary() {
+  const firstDayOfWeek = dayjs().startOf("week").toDate();
+  const lastDayOfWeek = dayjs().endOf("week").toDate();
+
+  const goalsCreatedUptToWeek = db.$with("goals_created_up_to_week").as(
+    db
+      .select({
+        id: goals.id,
+        title: goals.title,
+        desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+        createdAt: goals.createdAt,
+      })
+      .from(goals)
+      .where(lte(goals.createdAt, lastDayOfWeek))
+  );
+
+  const goalsCompletedInWeek = db.$with("goals_completed_in_week").as(
+    db
+      .select({
+        id: goalCompletions.id,
+        title: goals.title,
+        completedAt: goalCompletions.createdAt,
+        completedAtDate: sql`
+            DATE(${goalCompletions.createdAt})
+        `.as("completedAtDate"),
+      })
+      .from(goalCompletions)
+      .innerJoin(goals, eq(goals.id, goalCompletions.goalId))
+      .where(
+        and(
+          gte(goalCompletions.createdAt, firstDayOfWeek),
+          lte(goalCompletions.createdAt, lastDayOfWeek)
+        )
+      )
+  );
+
+  const goalsCompletedByWeekDay = db.$with("goals_completed_by_week_day").as(
+    db
+      .select({
+        completedAtDate: goalsCompletedInWeek.completedAtDate,
+        completions: sql`
+        JSON_AGG(
+            JSON_BUILD_OBJECT(
+                'id', ${goalsCompletedInWeek.id},
+                'title', ${goalsCompletedInWeek.title},
+                'completedAt', ${goalsCompletedInWeek.completedAt} 
+            )
+        )
+        `.as("completion"),
+      })
+      .from(goalsCompletedInWeek)
+      .groupBy(goalsCompletedInWeek.completedAtDate)
+  );
+
+  const result = await db
+    .with(goalsCreatedUptToWeek, goalsCompletedInWeek, goalsCompletedByWeekDay)
+    .select({
+      completed: sql`(SELECT COUNT(*) FROM ${goalsCompletedInWeek})`.mapWith(
+        Number
+      ),
+      total:
+        sql`(SELECT SUM(${goalsCreatedUptToWeek.desiredWeeklyFrequency}) FROM ${goalsCreatedUptToWeek})`.mapWith(
+          Number
+        ),
+      goalsPerDay: sql`
+            JSON_OBJECT_AGG(
+                ${goalsCompletedByWeekDay.completedAtDate},
+                ${goalsCompletedByWeekDay.completions}
+            )
+        `,
+    })
+    .from(goalsCompletedByWeekDay);
+
+  return {
+    summary: result,
+  };
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,59 +1,28 @@
+import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
 import {
   type ZodTypeProvider,
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
-import z from "zod";
-import { createGoal } from "../functions/create-goal";
-import { createGoalCompletion } from "../functions/create-goal-completion";
-import { getWeekPendingGoals } from "../functions/get-week-pending-goals";
+import { createCompletionRoute } from "./routes/create-completion";
+import { createGoalRoute } from "./routes/create-goal";
+import { getPendingGoalsRoute } from "./routes/get-pending-goals";
+import { getWeekSummaryRoute } from "./routes/get-week-summary";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.get("/pending-goals", async () => {
-  const { pendingGoals } = await getWeekPendingGoals();
-  return { pendingGoals };
-});
-
-app.post(
-  "/goals",
-  {
-    schema: {
-      body: z.object({
-        title: z.string(),
-        desiredWeeklyFrequency: z.number().int().min(1).max(7),
-      }),
-    },
-  },
-  async (request) => {
-    const { title, desiredWeeklyFrequency } = request.body;
-    await createGoal({
-      title,
-      desiredWeeklyFrequency,
-    });
-  }
-);
-
-app.post(
-  "/completions",
-  {
-    schema: {
-      body: z.object({
-        goalId: z.string(),
-      }),
-    },
-  },
-  async (request) => {
-    const { goalId } = request.body;
-    await createGoalCompletion({
-      goalId,
-    });
-  }
-);
+app.register(createGoalRoute);
+app.register(createCompletionRoute);
+app.register(getPendingGoalsRoute);
+app.register(getWeekSummaryRoute);
 
 app
   .listen({
