430054ea4dd3e38d0c8287351986a0ce3a3a55cc
Vitor Andes <vitor.andes.santos04@gmail.com>
2024-09-16 16:05:56 -0300

    removing seed

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,38 +1,38 @@
-import dayjs from "dayjs";
-import { client, db } from "../db";
-import { goalCompletions, goals } from "./schema";
+// import dayjs from "dayjs";
+// import { client, db } from "../db";
+// import { goalCompletions, goals } from "./schema";
 
-async function seed() {
-  await db.delete(goalCompletions);
-  await db.delete(goals);
+// async function seed() {
+//   await db.delete(goalCompletions);
+//   await db.delete(goals);
 
-  const [goal1, goal2] = await db
-    .insert(goals)
-    .values([
-      {
-        title: "caminhar",
-        desiredWeeklyFrequency: 1,
-      },
-      {
-        title: "nadar",
-        desiredWeeklyFrequency: 2,
-      },
-      {
-        title: "fazer exercícios",
-        desiredWeeklyFrequency: 1,
-      },
-    ])
-    .returning();
+//   const [goal1, goal2] = await db
+//     .insert(goals)
+//     .values([
+//       {
+//         title: "caminhar",
+//         desiredWeeklyFrequency: 1,
+//       },
+//       {
+//         title: "nadar",
+//         desiredWeeklyFrequency: 2,
+//       },
+//       {
+//         title: "fazer exercícios",
+//         desiredWeeklyFrequency: 1,
+//       },
+//     ])
+//     .returning();
 
-  const startOfWeek = dayjs().startOf("week");
+//   const startOfWeek = dayjs().startOf("week");
 
-  await db.insert(goalCompletions).values([
-    { goalId: goal1.id, createdAt: startOfWeek.toDate() },
-    { goalId: goal2.id, createdAt: startOfWeek.add(1, "day").toDate() },
-  ]);
-}
+//   await db.insert(goalCompletions).values([
+//     { goalId: goal1.id, createdAt: startOfWeek.toDate() },
+//     { goalId: goal2.id, createdAt: startOfWeek.add(1, "day").toDate() },
+//   ]);
+// }
 
-seed().then(() => {
-  console.log("🌱 Database seeded successfully!");
-  client.end();
-});
+// seed().then(() => {
+//   console.log("🌱 Database seeded successfully!");
+//   client.end();
+// });
